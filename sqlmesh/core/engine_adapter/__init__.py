98f63edfd10d19eece7e5c4b8e5ec38c45596aaa
Awanish Gupta <awanishmac@dhcp-9-81-16-128.lkw-in.ibm.com>
2026-02-25 11:56:13 +0530

    Added DB2 adapter changes

--- sqlmesh/core/engine_adapter/__init__.py
+++ sqlmesh/core/engine_adapter/__init__.py
@@ -20,6 +20,7 @@ from sqlmesh.core.engine_adapter.trino import TrinoEngineAdapter
 from sqlmesh.core.engine_adapter.athena import AthenaEngineAdapter
 from sqlmesh.core.engine_adapter.risingwave import RisingwaveEngineAdapter
 from sqlmesh.core.engine_adapter.fabric import FabricEngineAdapter
+from sqlmesh.core.engine_adapter.db2_proper import DB2EngineAdapter
 
 DIALECT_TO_ENGINE_ADAPTER = {
     "hive": SparkEngineAdapter,
@@ -37,6 +38,7 @@ DIALECT_TO_ENGINE_ADAPTER = {
     "athena": AthenaEngineAdapter,
     "risingwave": RisingwaveEngineAdapter,
     "fabric": FabricEngineAdapter,
+    "db2": DB2EngineAdapter,
 }
 
 DIALECT_ALIASES = {
